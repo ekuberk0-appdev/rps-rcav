8726858480588576210f759dfe81280f74433698
edita21 <ekuberk0@chicagobooth.edu>
2021-07-10 20:30:35 +0000

    v3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,20 @@ class ApplicationController < ActionController::Base
 render ({ :template => "game_templates/user_rock.html.erb"})
   end
   def play_paper
+
+     comp_move = ["rock", "paper", "scissors"].sample 
+
+    
+     if comp_move =="rock"
+       outcome = "won"
+    
+     elsif comp_move =="paper" 
+        outcome = "tied"
+    
+     elsif comp_move == "scissors" 
+        outcome = "lost"
+    end    
+
     render({ :template => "game_templates/user_paper.html.erb"})
   end
 
